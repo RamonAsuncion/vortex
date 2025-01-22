7202b20f12aecad415f77da6f1abf957031d918b
Ramon Asuncion <ramon@b1t.dev>
2025-01-22 13:01:28 -0500

    Properly cast a spell.

--- js/castSpell.js
+++ js/castSpell.js
@@ -1,4 +1,10 @@
 function castSpell() {
-  document.getElementById("spellResult").innerText =
-    "Your spell has been cast successfully!";
+  const spell = document.getElementById("spellResult");
+  spell.innerText = "Your spell has been cast successfully!";
+  setTimeout(() => {
+    spell.innerText = "";
+  }, 2000);
 }
+
+const button = document.getElementById("castSpellButton");
+button.onclick = castSpell;

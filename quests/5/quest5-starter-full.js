9fe67ac53f8801b2e296d0dc39e2e6c9b68f59ec
Ramon Asuncion <ramon@b1t.dev>
2025-03-24 08:26:06 -0400

    Remove period.

--- quests/5/quest5-starter-full.js
+++ quests/5/quest5-starter-full.js
@@ -389,7 +389,7 @@ function _0x365e() {
     "clear",
     "createTranslator",
     "STORAGE",
-    "./assets/shapes/box.polygon",
+    "/assets/shapes/box.polygon",
     "createGeometry",
     "dense",
     "requestAdapter",
@@ -423,7 +423,7 @@ function _0x365e() {
     "human",
     "616gJerQx",
     "createShaderModule",
-    "./assets/shapes/star.polygon",
+    "/assets/shapes/star.polygon",
     "setRequestHeader",
     "createPoint",
     "font",
@@ -440,7 +440,7 @@ function _0x365e() {
     "float32x",
     "_polygon",
     "responseType",
-    "./assets/shapes/dense.polygon",
+    "/assets/shapes/dense.polygon",
     "WebGPU\x20is\x20not\x20supported\x20in\x20this\x20browser.",
     "motorNorm",
     "status",
@@ -923,7 +923,7 @@ async function init() {
           ((_0x3a87b0 = _0x377dd4(0x14c)),
           _0x5af11e(_0x3a87b0),
           (_0x4da8b3[_0x377dd4(0x1b0)][_0x377dd4(0x14a)] =
-            "./assets/shapes/circle.polygon"),
+            "/assets/shapes/circle.polygon"),
           await _0x18ca27());
         break;
       case "h":
@@ -931,7 +931,7 @@ async function init() {
           ((_0x3a87b0 = _0x377dd4(0x19e)),
           _0x5af11e(_0x3a87b0),
           (_0x4da8b3["_polygon"][_0x377dd4(0x14a)] =
-            "./assets/shapes/human.polygon"),
+            "/assets/shapes/human.polygon"),
           await _0x18ca27());
         break;
       case "s":
